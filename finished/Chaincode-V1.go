c6d38a0b03984cea2f705e4cd30375b9b38977d4
Melitta27 <melitta_dsouza@persistent.com>
2017-06-19 17:41:56 +0530

    update 17

--- finished/Chaincode-V1.go
+++ finished/Chaincode-V1.go
@@ -124,6 +124,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
+	fmt.Println(valAsbytes)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
