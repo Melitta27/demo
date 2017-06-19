568dd9dd6cf88fa85e2c7072b87b6d1bdd3c6887
Melitta27 <melitta_dsouza@persistent.com>
2017-06-19 18:08:25 +0530

    update 18

--- finished/Chaincode-V1.go
+++ finished/Chaincode-V1.go
@@ -6,10 +6,6 @@ import (
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
-const (
-	UserPrefix	= "USER_"
-	
-)
 
 //Patient Struct
   type Patient struct {
@@ -32,7 +28,7 @@ func main() {
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	var key string
 	var err error
-         key = UserPrefix + args[0]
+         key = args[0]
 	if len(args) != 2{
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
@@ -117,14 +113,20 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
-
+	var key1 string
+	var length int 
+	
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
-	fmt.Println(valAsbytes)
+	key1 = args[1]
+	val, err :=stub.GetState(key1)
+	length = len(valAsbytes)
+	valAsbytes[length] = val
+	
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
